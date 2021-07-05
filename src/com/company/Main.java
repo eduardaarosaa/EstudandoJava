7420aec29b254f62d90ae96231d92433c33833a5
Eduarda <eduarda@houpa.app>
2021-07-05 15:18:12 -0300

    Sistema de hora

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Date;
+public class Main {
+    //Hora do sistema
+    public static void main(String[] args) {
+        //Criando um objeto
+        Date relogio  = new Date();
+        System.out.println("A hora do sistema é ");
+        System.out.println(relogio.toString());
+
+    }
+}
