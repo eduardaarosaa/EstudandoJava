97e00eb27372deeffbf5d7678269b7ce09034d02
Eduarda <eduarda@houpa.app>
2021-07-05 15:30:31 -0300

    Resolvendo 2 exercicios do curso em video

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
+import java.awt.*;
 import java.util.Date;
+import java.util.Locale;
+
 public class Main {
     //Hora do sistema
     public static void main(String[] args) {
@@ -7,6 +10,19 @@ public class Main {
         Date relogio  = new Date();
         System.out.println("A hora do sistema é ");
         System.out.println(relogio.toString());
-
+        setIdioma();
+        setResolucao();
+    }
+    //Idioma do sistema operacional
+    public static void setIdioma(){
+        Locale idioma = Locale.getDefault();
+        System.out.println(idioma.getDisplayLanguage());
+    }
+    //Pegar resoluca da tela
+    public static void setResolucao(){
+        Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+        int largura = (int) tela.getWidth();
+        int altura = (int) tela.getHeight();
+        System.out.println("Sua tela tem resolução " + largura + " x " + altura);
     }
 }
