d1ef0d605214148babf579f05e243136a1e0aefb
Eduarda <eduarda@houpa.app>
2021-07-20 23:34:01 -0300

    Adicionando mais tipos de conversoes de dados

--- TesteTipos.java
+++ TesteTipos.java
@@ -15,8 +15,20 @@ public class TesteTipos {
      */
     public static void main(String args[]) {
         // TODO code application logic here
+        //Convertendo idade que é int para String
         int idade = 30; 
         String valor = Integer.toString(idade);
         System.out.println(valor);
+        
+        //Convertendo um valor String para int 
+        String valorText = "50"; 
+        int numero = Integer.parseInt(valorText);
+        System.out.println(numero);
+        
+        //Convertendo um valor em String para Float
+        String valorFloat = "3.5";
+        float valorQuebrado = Float.parseFloat(valorFloat);
+        System.out.println(valorQuebrado);
+        
     }
 }
