714b0c19c1642c36af2b4d43c826079a99557ffd
Eduarda <eduarda@houpa.app>
2021-07-20 23:27:56 -0300

    Add o teste de tipos

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -1,6 +1,5 @@
 package tiposprimitivos; 
-
-
+import java.util.Scanner;
 /**
  * @author EduardaRosa
  */
@@ -10,16 +9,18 @@ public class TiposPrimitivos {
         * @param args the command line arguments
         */
         public static void main(String[] args){
-            
+           
+            Scanner teclado = new Scanner(System.in);
             //Declarando variaveis e tipos
-            float nota = 8.5f;
-            String nome = "Duda";
-            
+            System.out.println("Digite o nome do aluno: ");
+            String nome = teclado.nextLine();
+            System.out.println("Digite a nota do aluno: ");
+            float nota = teclado.nextFloat();
             System.out.println("A nota é " + nota);
             //Para trabalhar com casas decimais
             System.out.printf("A nota é %f \n", nota);
             System.out.printf("A nota é %.2f \n", nota);
             //Mais um formato de escrita na tela
-            System.out.format("A nota de %s é %.4f \n", nome , nota);
+            System.out.format("A nota de %s é %.1f \n", nome , nota);
         }
 }
